8f3f26c1892e94741113e1353ecac9c0bcd9b13a
徐来 <386710057@qq.com>
2025-06-04 11:05:40 +0800

    simples

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,6 +1,6 @@
 // THIS FILE IS PART OF Xunet.MiniApi PROJECT
 // THE Xunet.MiniApi PROJECT IS AN OPENSOURCE LIBRARY LICENSED UNDER THE MIT License.
-// COPYRIGHTS (C) ���� ALL RIGHTS RESERVED.
+// COPYRIGHTS (C) 徐来 ALL RIGHTS RESERVED.
 // GITHUB: https://github.com/shelley-xl/Xunet.MiniApi
 
 var builder = WebApplication.CreateBuilder(args);
