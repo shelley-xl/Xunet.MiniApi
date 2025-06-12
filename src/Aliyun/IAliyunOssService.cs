0d63f2c24e755388c4e4b733fb6f6d2f69ebb0df
徐来 <386710057@qq.com>
2025-06-12 17:13:19 +0800

    阿里云对象存储服务完善

--- src/Aliyun/IAliyunOssService.cs
+++ src/Aliyun/IAliyunOssService.cs
@@ -10,5 +10,54 @@ namespace Xunet.MiniApi.Aliyun;
 /// </summary>
 public interface IAliyunOssService
 {
+    /// <summary>
+    /// 创建任务
+    /// </summary>
+    /// <returns></returns>
+    string CreateTask();
 
+    /// <summary>
+    /// 获取进度
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <returns></returns>
+    List<UploadProgressDto> GetProgress(string taskId);
+
+    /// <summary>
+    /// 上传文件
+    /// </summary>
+    /// <param name="taskId">任务id</param>
+    /// <param name="content">文件流</param>
+    /// <param name="key">文件key</param>
+    /// <param name="bucket">bucket</param>
+    /// <param name="area">地区</param>
+    /// <returns>文件地址</returns>
+    string PutObject(string taskId, Stream? content, string key, string bucket, string area);
+
+    /// <summary>
+    /// 上传文件
+    /// </summary>
+    /// <param name="content">文件流</param>
+    /// <param name="key">文件key</param>
+    /// <param name="bucket">bucket</param>
+    /// <param name="area">地区</param>
+    /// <returns>文件地址</returns>
+    string PutObject(Stream? content, string key, string bucket, string area);
+
+    /// <summary>
+    /// 删除文件
+    /// </summary>
+    /// <param name="keys">文件key列表</param>
+    /// <param name="bucket">bucket</param>
+    /// <param name="area">地区</param>
+    void DeleteObject(IList<string> keys, string bucket, string area);
+
+    /// <summary>
+    /// 查找文件key列表
+    /// </summary>
+    /// <param name="prefix">前缀</param>
+    /// <param name="bucket">bucket</param>
+    /// <param name="area">地区</param>
+    /// <returns></returns>
+    IList<string> GetKeys(string prefix, string bucket, string area);
 }
--- src/Aliyun/Oss/AliyunOssService.cs
+++ src/Aliyun/Oss/AliyunOssService.cs
@@ -8,7 +8,157 @@ namespace Xunet.MiniApi.Aliyun.Oss;
 /// <summary>
 /// 阿里云对象存储服务
 /// </summary>
-internal class AliyunOssService : IAliyunOssService
+internal class AliyunOssService(IConfiguration config) : IAliyunOssService
 {
+    /// <summary>
+    /// 存储任务的容器
+    /// </summary>
+    static readonly ConcurrentDictionary<string, List<UploadProgressDto>> _progresses = new();
 
+    string? AccessKeyId => config["AliyunOssSettings:AccessKeyId"];
+
+    string? AccessKeySecret => config["AliyunOssSettings:AccessKeySecret"];
+
+    /// <summary>
+    /// 创建任务
+    /// </summary>
+    /// <returns></returns>
+    public string CreateTask()
+    {
+        var taskId = Guid.NewGuid().ToString("N");
+
+        _progresses.TryAdd(taskId, []);
+
+        // 自动清理缓存：设置2小时后自动移除进度
+        JobManager.AddJob(() => _progresses.TryRemove(taskId, out _), (x) =>
+        {
+            x.WithName($"remove:progress:{taskId}");
+            x.ToRunOnceIn(7200).Seconds();
+        });
+
+        return taskId;
+    }
+
+    /// <summary>
+    /// 获取进度
+    /// </summary>
+    /// <param name="taskId"></param>
+    /// <returns></returns>
+    public List<UploadProgressDto> GetProgress(string taskId)
+    {
+        if (_progresses.TryGetValue(taskId, out var progress))
+        {
+            return progress;
+        }
+        return [];
+    }
+
+    /// <summary>
+    /// 上传文件
+    /// </summary>
+    /// <param name="taskId">任务id</param>
+    /// <param name="content">文件流</param>
+    /// <param name="key">文件key</param>
+    /// <param name="bucket">bucket</param>
+    /// <param name="area">地区</param>
+    /// <returns>文件地址</returns>
+    public string PutObject(string taskId, Stream? content, string key, string bucket, string area)
+    {
+        if (!_progresses.TryGetValue(taskId, out var progress))
+        {
+            throw new ArgumentException($"任务{taskId}不存在");
+        }
+
+        var index = progress.Count;
+
+        progress.Add(new UploadProgressDto());
+
+        var request = new PutObjectRequest(bucket, key, content);
+
+        request.StreamTransferProgress += (object? sender, StreamTransferProgressArgs args) =>
+        {
+            progress[index].BytesUploaded = args.TransferredBytes;
+            progress[index].TotalBytes = args.TotalBytes;
+        };
+
+        var endpoint = $"oss-cn-{area}.aliyuncs.com";
+        var client = new OssClient(endpoint, AccessKeyId, AccessKeySecret);
+
+        var result = client.PutObject(request);
+
+        if (result.HttpStatusCode != HttpStatusCode.OK)
+        {
+            throw new OssException("上传文件失败");
+        }
+
+        return $"https://{bucket}.{endpoint}/{key}";
+    }
+
+    /// <summary>
+    /// 上传文件
+    /// </summary>
+    /// <param name="content">文件流</param>
+    /// <param name="key">文件key</param>
+    /// <param name="bucket">bucket</param>
+    /// <param name="area">地区</param>
+    /// <returns>文件地址</returns>
+    public string PutObject(Stream? content, string key, string bucket, string area)
+    {
+        var request = new PutObjectRequest(bucket, key, content);
+        
+        var endpoint = $"oss-cn-{area}.aliyuncs.com";
+        var client = new OssClient(endpoint, AccessKeyId, AccessKeySecret);
+
+        var result = client.PutObject(request);
+
+        if (result.HttpStatusCode != HttpStatusCode.OK)
+        {
+            throw new OssException("上传文件失败");
+        }
+
+        return $"https://{bucket}.{endpoint}/{key}";
+    }
+
+    /// <summary>
+    /// 删除文件
+    /// </summary>
+    /// <param name="keys">文件key列表</param>
+    /// <param name="bucket">bucket</param>
+    /// <param name="area">地区</param>
+    public void DeleteObject(IList<string> keys, string bucket, string area)
+    {
+        var request = new DeleteObjectsRequest(bucket, keys);
+
+        var endpoint = $"oss-cn-{area}.aliyuncs.com";
+        var client = new OssClient(endpoint, AccessKeyId, AccessKeySecret);
+        
+        var result = client.DeleteObjects(request);
+
+        if (result.HttpStatusCode != HttpStatusCode.OK)
+        {
+            throw new OssException("删除文件失败");
+        }
+    }
+
+    /// <summary>
+    /// 查找文件key列表
+    /// </summary>
+    /// <param name="prefix">前缀</param>
+    /// <param name="bucket">bucket</param>
+    /// <param name="area">地区</param>
+    /// <returns></returns>
+    public IList<string> GetKeys(string prefix, string bucket, string area)
+    {
+        var endpoint = $"oss-cn-{area}.aliyuncs.com";
+        var client = new OssClient(endpoint, AccessKeyId, AccessKeySecret);
+
+        var result = client.ListObjects(bucket, prefix);
+
+        if (result.HttpStatusCode != HttpStatusCode.OK)
+        {
+            throw new OssException("查找文件失败");
+        }
+
+        return result.ObjectSummaries.Select(x => x.Key).ToList();
+    }
 }
