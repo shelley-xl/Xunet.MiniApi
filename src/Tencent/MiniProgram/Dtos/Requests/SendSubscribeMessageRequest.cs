0b5ae9c4629cb52bae96bc65ea6b259679085c1d
徐来 <386710057@qq.com>
2025-07-28 14:53:03 +0800

    修复已知问题

--- src/Tencent/MiniProgram/Dtos/Requests/SendSubscribeMessageRequest.cs
+++ src/Tencent/MiniProgram/Dtos/Requests/SendSubscribeMessageRequest.cs
@@ -14,7 +14,7 @@ public class SendSubscribeMessageRequest
     /// 所需下发的订阅模板id
     /// </summary>
     [JsonPropertyName("template_id")]
-    public string? TemplateId { get; set; }
+    public required string TemplateId { get; set; }
 
     /// <summary>
     /// 点击模板卡片后的跳转页面，仅限本小程序内的页面。支持带参数,（示例index?foo=bar）。该字段不填则模板无跳转
@@ -26,13 +26,13 @@ public class SendSubscribeMessageRequest
     /// 接收者（用户）的 openid
     /// </summary>
     [JsonPropertyName("touser")]
-    public string? ToUser { get; set; }
+    public required string ToUser { get; set; }
 
     /// <summary>
     /// 模板内容，格式形如{ "phrase3": { "value": "审核通过" }, "name1": { "value": "订阅" }, "date2": { "value": "2019-12-25 09:42" } }
     /// </summary>
     [JsonPropertyName("data")]
-    public object? Data { get; set; }
+    public required object Data { get; set; }
 
     /// <summary>
     /// 跳转小程序类型：developer为开发版；trial为体验版；formal为正式版；默认为正式版
@@ -50,5 +50,5 @@ public class SendSubscribeMessageRequest
     /// 接口调用凭证
     /// </summary>
     [JsonIgnore]
-    public string? AccessToken { get; set; }
+    public required string AccessToken { get; set; }
 }
--- src/Tencent/WeixinMp/Dtos/Requests/SendTemplateMessageRequest.cs
+++ src/Tencent/WeixinMp/Dtos/Requests/SendTemplateMessageRequest.cs
@@ -14,13 +14,13 @@ public class SendTemplateMessageRequest
     /// 接收者openid
     /// </summary>
     [JsonPropertyName("touser")]
-    public string? ToUser { get; set; }
+    public required string ToUser { get; set; }
 
     /// <summary>
     /// 模板ID
     /// </summary>
     [JsonPropertyName("template_id")]
-    public string? TemplateId { get; set; }
+    public required string TemplateId { get; set; }
 
     /// <summary>
     /// 模板跳转链接（海外帐号没有跳转能力）
@@ -32,13 +32,13 @@ public class SendTemplateMessageRequest
     /// 模板跳小程序所需数据，不需跳小程序可不用传该数据
     /// </summary>
     [JsonPropertyName("miniprogram")]
-    public Dictionary<string, string>? MiniProgram { get; set; }
+    public object? MiniProgram { get; set; }
 
     /// <summary>
     /// 模板数据
     /// </summary>
     [JsonPropertyName("data")]
-    public Dictionary<string, string>? Data { get; set; }
+    public required object Data { get; set; }
 
     /// <summary>
     /// 防重入id。对于同一个openid + client_msg_id, 只发送一条消息,10分钟有效,超过10分钟不保证效果。若无防重入需求，可不填
@@ -50,5 +50,5 @@ public class SendTemplateMessageRequest
     /// 接口调用凭证
     /// </summary>
     [JsonIgnore]
-    public string? AccessToken { get; set; }
+    public required string AccessToken { get; set; }
 }
